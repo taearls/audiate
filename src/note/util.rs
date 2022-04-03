08754ee85eece987054c010b9f9194f9dad4c6da
Tyler Earls <tyler.a.earls@gmail.com>
2022-04-03 00:29:57 -0500

    refactor uppercase_first_char fn, add more unit tests

--- src/note/util.rs
+++ src/note/util.rs
@@ -1,8 +1,13 @@
 pub fn uppercase_first_char(input: &str) -> String {
+    let input = input.trim();
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+        Some(f) => {
+            let mut upper = f.to_uppercase().collect::<String>();
+            upper.push_str(&input[1..]);
+            upper
+        }
     }
 }
 
@@ -22,6 +27,7 @@ mod uppercase_first_char_tests {
     #[test]
     fn uppercase_first_char_returns_empty_str() {
         test_case("", "");
+        test_case("   ", "");
     }
 
     #[test]
@@ -34,5 +40,12 @@ mod uppercase_first_char_tests {
         test_case("apple", "Apple");
         test_case("peach", "Peach");
         test_case("orange", "Orange");
+    }
+
+    #[test]
+    fn uppercase_first_char_works_for_multi_char_str_nonalphabetic() {
+        test_case("1  ", "1");
+        test_case("12", "12");
+        test_case("4 bananas", "4 bananas");
     }
 }
